f136981fb3a090ca1beff9dbad8b9a6fe459e479
Hit Vaghani <68742522+hitvaghani26@users.noreply.github.com>
2023-08-01 10:20:37 +0530

    change whole index.js

--- index.js
+++ index.js
@@ -5,95 +5,70 @@ const User = require("./models/user.model")
 const session = require('express-session')
 const MongoStore = require('connect-mongo');
 const cookieParser = require("cookie-parser");
-const bycrypt= require("bcrypt");
+const bycrypt = require("bcrypt");
 const PORT = process.env.PORT || 5500;
 require("dotenv").config()
 
-
 const mongoDBUrl = process.env.MONGODBURL;
 const sessionKey = process.env.SESSIONKEY;
-
-
 
 const login_route = require("./routes/login")
 const register_route = require("./routes/register")
 
 const app = express();
-// const store = new MongoDBStore({
-//     uri: "mongodb+srv://vaghaniheet123:heet737@cluster0.9rdxqou.mongodb.net/plantweb",
-//     collection: "sessions"
-// });
+
 const corsOptions = {
-  origin: 'https://64c87ed8457d0a327ec0b683--stellular-naiad-bd243e.netlify.app',
+  origin: 'https://64c88cc27dd429379aeacad4--stellular-naiad-bd243e.netlify.app',
   credentials: true,
 };
 
 app.options('*', cors(corsOptions)); // Enable preflight requests for all routes
 app.use(cors(corsOptions)); // Apply CORS to all routes
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser('secret'));
 
-
-
 app.use(session({
-    secret: sessionKey,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-        mongoUrl:  mongoDBUrl,
-        collection: "sessions" 
-    }),
-    cookie: {
-       sameSite: 'none',
-        maxAge: 60 * 60 * 1000,
-    },
+  secret: sessionKey,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: mongoDBUrl,
+    collection: "sessions"
+  }),
+  cookie: {
+    sameSite: 'none', // Set this based on your requirements
+    secure: true, // Set this to true if your frontend is using HTTPS
+    maxAge: 60 * 60 * 1000,
+  },
 }));
 
+app.use("/login", login_route);
+app.use("/register", register_route);
 
-app.use("/login", login_route)
-app.use("/register", register_route)
-
-
-
-
-mongoose.connect("mongodb+srv://vaghaniheet123:heet737@cluster0.9rdxqou.mongodb.net/plantweb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-
+mongoose.connect(mongoDBUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-
-
 
 app.get("/check", async (req, res) => {
-    console.log(req.sessionID);
-if(req.session.user){
-    res.send({msg:"you don't have to login", user:req.session})
-}
-    else{
-        res.send({msg : "go to login pagex  "})
-    }
-})
-
-
-
-app.post('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(500).json({ error: 'Failed to log out.' });
-        }
-        res.clearCookie('connect.sid'); 
-        res.json({ message: 'Logged out successfully.' });
-    });
+  console.log(req.sessionID);
+  if (req.session.user) {
+    res.send({ msg: "you don't have to login", user: req.session });
+  } else {
+    res.send({ msg: "go to login page" });
+  }
 });
 
-
-
-
-
-
-
-
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to log out.' });
+    }
+    res.clearCookie('connect.sid');
+    res.json({ message: 'Logged out successfully.' });
+  });
+});
 
 app.listen(PORT, () => {
-    console.log("app is listing at 5500");
-})
+  console.log("app is listening at 5500");
+});
